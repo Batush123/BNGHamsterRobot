7e250f83590318cc99e164764b91c9ccd0d7839b
galmalca <galmaalca10@gmail.com>
2017-08-10 16:40:09 +0300

    final

--- Debug/ConfigurationManager.d
+++
@@ -1,8 +0,0 @@
-ConfigurationManager.o: ../ConfigurationManager.cpp \
- ../ConfigurationManager.h ../Location.h ../Globals.h
-
-../ConfigurationManager.h:
-
-../Location.h:
-
-../Globals.h:
--- Debug/LocalizationManager.d
+++
@@ -1,8 +0,0 @@
-LocalizationManager.o: ../LocalizationManager.cpp \
- ../LocalizationManager.h ../Particle.h ../Location.h
-
-../LocalizationManager.h:
-
-../Particle.h:
-
-../Location.h:
